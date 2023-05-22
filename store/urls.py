c02149ca049155d2177a77c46568d61ff4f1885b
Asirius <darkhotik2015@yandex.ru>
2023-05-22 15:18:57 +0300

    Добавлен url для регистрации аккаунтов через социальные приложения

--- store/urls.py
+++ store/urls.py
@@ -24,7 +24,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.IndexView.as_view(), name='index'),
     path('products/', include('products.urls', namespace='products')),
-    path('users/', include('users.urls', namespace='users'))
+    path('users/', include('users.urls', namespace='users')),
+
+    path('accounts/', include('allauth.urls')),
 ]
 
 if settings.DEBUG:
