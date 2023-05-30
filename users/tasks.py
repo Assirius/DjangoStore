93bb040925481ce61e58e2a762a7804ba107dd08
assirius <darkhotik2015@yadex.ru>
2023-05-30 21:29:47 +0300

    tasks for email verification using celery

---
+++ users/tasks.py
@@ -0,0 +1,16 @@
+import uuid
+from datetime import timedelta
+
+from celery import shared_task
+
+from django.utils.timezone import now
+
+from users.models import User, EmailVerification
+
+
+@shared_task
+def send_email_verification(user_id):
+    user = User.objects.get(id=user_id)
+    expirations = now() + timedelta(hours=48)
+    verification = EmailVerification.objects.create(code=uuid.uuid4(), user=user, expiration=expirations)
+    verification.send_verification_email()
