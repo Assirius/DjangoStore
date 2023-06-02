5e5c5bb9fc3534e340d96d9e6b68e0b93d8a8d52
assirius <darkhotik2015@yadex.ru>
2023-06-02 16:24:26 +0300

    добавлены путь в приложение orders для работы с заказами и путь для вебхука системы оплаты

--- store/urls.py
+++ store/urls.py
@@ -18,14 +18,16 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-from products import views
+from orders.views import stripe_webhook_view
+from products.views import IndexView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.IndexView.as_view(), name='index'),
+    path('', IndexView.as_view(), name='index'),
     path('products/', include('products.urls', namespace='products')),
     path('users/', include('users.urls', namespace='users')),
     path('orders/', include('orders.urls', namespace='orders')),
+    path('webhook/stripe/', stripe_webhook_view, name='stripe-webhook'),
 
     path('accounts/', include('allauth.urls')),
 ]
