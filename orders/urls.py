ee3e89e3ea73b745c0a8cb7dd812ccb443622360
assirius <darkhotik2015@yadex.ru>
2023-06-02 16:18:58 +0300

    добавлены пути к контроллерам работы с заказами

--- orders/urls.py
+++ orders/urls.py
@@ -6,4 +6,8 @@ app_name = 'orders'
 
 urlpatterns = [
     path('order-create', views.OrderCreateView.as_view(), name='order-create'),
+    path('', views.OrderListView.as_view(), name='orders-list'),
+    path('order/<int:pk>/', views.OrderDetailView.as_view(), name='order-detail'),
+    path('order-success', views.SuccessTemplateView.as_view(), name='order-success'),
+    path('order-cancel', views.CancelTemplateView.as_view(), name='order-cancel'),
 ]
