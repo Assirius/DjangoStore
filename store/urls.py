f45cd6bdb8c826dd7677b70d5748fbd1399c7fff
assirius <darkhotik2015@yadex.ru>
2023-06-07 20:31:39 +0300

    добавлены пути для django rest

--- store/urls.py
+++ store/urls.py
@@ -17,6 +17,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.authtoken.views import obtain_auth_token
 
 from orders.views import stripe_webhook_view
 from products.views import IndexView
@@ -28,6 +29,9 @@ urlpatterns = [
     path('users/', include('users.urls', namespace='users')),
     path('orders/', include('orders.urls', namespace='orders')),
     path('webhook/stripe/', stripe_webhook_view, name='stripe-webhook'),
+
+    path('api/', include('api.urls', namespace='api')),
+    path('api-token-auth/', obtain_auth_token),
 
     path('accounts/', include('allauth.urls')),
 ]
